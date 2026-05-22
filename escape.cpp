9128d40425b37e9ccddd7cbd20e3954d57dd75bc
s1140453-a11y <s1140453@dmhs.kh.edu.tw>
2026-05-22 11:03:40 +0800

    Create escape.cpp

---
+++ escape.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	count << "1.hello" << endl
+	count << "2.he llo" << endl
+	count << "3.he/tllo" << endl
+	count << "4.he/nllo" << endl
+	count << "n"5.hello"" << endl
+	count << "/'6.hello''" << endl
+	count << "// 7.hello//" << endl
+	return 0;
+}
