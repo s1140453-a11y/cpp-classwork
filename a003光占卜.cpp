c6536a705a743691af6847a1fddc6eeb2ee894c7
s1140453-a11y <s1140453@dmhs.kh.edu.tw>
2026-09-15 11:47:08 +0800

    Create a003光占卜.cpp

---
+++ a003光占卜.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+
+	int M, D, S;
+
+	S=(M * 2 + D) % 3 ;
+
+	cin >> M >> D;
+
+	S = (M * 2 + D)% 3;
+
+	if (S==0)
+
+			cout << "普通" << endl;
+
+	if (S==1)
+
+			cout << "吉" << endl;
+
+	if (S==2)
+
+			cout << "大吉" << endl;
+
+	return 0;
+
+}
